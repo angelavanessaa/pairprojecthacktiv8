bf847f3d5eb412eaefb990af6fd6e92c9a9d1577
Angela Vanessa <hansavanessa@gmail.com>
2019-10-23 19:47:50 +0700

    many to many not done

--- controllers/CinemaMovieController.js
+++ controllers/CinemaMovieController.js
@@ -1,12 +1,54 @@
 const CinemaMovie = require('../models').CinemaMovie;
+const Movie = require('../models').Movie;
 
 class CinemaMovieController {
     static index(req, res) {
-        res.render('movie/bookMovie')
+        Movie.findOne(req.param.id)
+            .then( movie => {
+                let seatMap = CinemaMovieController.generateSeatMap(movie.dataValues);
+                res.render('movie/bookMovie', { movie, seatMap })
+            })
+            .catch( err => {
+                res.send(err);
+            })
     }
 
     static create(req, res) {
-        res.send(req.body)
+        for (let i = 0; i < req.body.orders.length; i++) {
+            let order = req.body.orders[i];
+            CinemaMovie.create({
+                CinemaId : 1,
+                MovieId : req.body.movieData.id,
+                UserId : 1,
+                booking_code: 'asdasda',
+                seatNumber : order
+            })
+        }
+        // console.log(req.body)
+        res.send({ redirect: 'http://localhost:3000/' })
+    }
+
+    static generateSeatMap(movie){
+        let map = [];
+        let num = movie.maxtickets;
+        let str = '';
+
+        for (let i = 0; i < num; i++) {
+            if (i % 5 == 0 && i != 0 && i % 10 != 0) {
+                str += '_'
+            }
+
+            if (i % 10 == 0 && i != 0) {
+                map.push(str);
+                str = 'e';
+            } else {
+                str += 'e';
+            }
+        }
+
+        let lastrow = 'eeeeeeeeeee';
+        map[map.length - 1] = lastrow;
+        return map;
     }
 }
 

bb82097414972c88dc0b7939eeb5fe51ff9d18ff
Angela Vanessa <hansavanessa@gmail.com>
2019-10-24 22:34:43 +0700

    config

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const UserController = require('./controllers/UserController');
 const MovieController = require('./controllers/MovieController');
 const TransactionController = require('./controllers/TransactionController');
